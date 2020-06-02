5ea9af27900333631323c3f89b1b365262a3d4e0
yutamasumoto <yuta.masumoto@di-v.co.jp>
2020-06-02 16:56:21 +0900

    change deploy

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,9 +2,9 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="MessageBox">
+        `<div class="MessageBox" data-message-id=${message.id}>
           <div class="MessageInfo">
-            <div class="MessageInfo__UserName">
+            <div class="MessageInfo__userName">
               ${message.user_name}
             </div>
             <div class="MessageInfo__Date">
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="MessageBox">
+      `<div class="MessageBox" data-message-id=${message.id}>
         <div class="MessageInfo">
           <div class="MessageInfo__UserName">
             ${message.user_name}
@@ -56,14 +56,13 @@ $(function(){
       let html = buildHTML(data);
       $('.MainChat__MessageField').append(html);      
       $('form')[0].reset();
-      $('.box').animate({'height' : '200px'});
-      $('.MainChat__MessageField').animate({ scrollTop: $('.MainChat__MessageField')[0].scrollHeight});
-    })
-    .always(function(){
-      $('.submit-btn').prop('disabled', false);
+      $('.MainChat__MessageField').animate({ scrollTop: $('.MainChat__MessageField')[0].scrollHeight});      
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
-    });
+    })
+    .always(function(){
+      $(".submit-btn").removeAttr("disabled");
+    });      
   });
 });
